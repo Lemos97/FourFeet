c2f8ddebb31094bea6c9cba4970735a1ad9fc01a
LEMOS-PC\Lemos <brunomina1997@hotmail.com>
2017-09-19 21:26:13 +0100

    Revert "Revert "Xamarin App Initial Commit""
    This reverts commit 1cecccdc10b1db831319a6ba3cb97de332a72169.

---
+++ FourFeetApp/FourFeetApp/FourFeetApp/WelcomeScreen.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FourFeetApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomeScreen : ContentPage
+    {
+        public WelcomeScreen()
+        {
+            InitializeComponent();
+        }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
