9ebde6998fdf9d3523fdebb2ac74bc60e3260cbf
Lemos97 <brunomina1997@hotmail.com>
2017-09-23 20:59:47 +0100

    Added a progressive endpoint to trigger a progressive kind of pattern

--- Scripts/WebService/Main.py
+++ Scripts/WebService/Main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-
+import time
 from colorPattern import rookie, intermediate, advanced
 
 # from thread import start_new_thread
@@ -11,17 +11,32 @@ app = Flask(__name__)
 @app.route('/exercise', methods=['GET'])
 def level():
     exc_level = request.args.get('level')
+    progressive = request.args.get('progressive')
+    # ip:5000/exercise?level=rookie ||
+    # ip:5000/exercise?level=intermediate ||
+    # ip:5000/exercise?level=advanced
     if exc_level:
         if exc_level == 'rookie':
-            rookie()
+            return rookie()
         elif exc_level == 'intermediate':
-            intermediate()
+            return intermediate()
         elif exc_level == 'advanced':
+            return advanced()
+
+    # ip:5000/exercise?progressive=yes || ip:5000/exercise?progressive=Yes
+    if progressive:
+        if progressive == 'yes' or progressive == 'Yes':
+            rookie()
+            time.sleep(5)
+            intermediate()
+            time.sleep(7)
             advanced()
-        else:
-            return 404
+            return "## Progressive mode ended ##"
+
+    return 404
 
 
 if __name__ == "__main__":
 
     app.run(host="0.0.0.0")
+
