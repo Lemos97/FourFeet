45ec8935def1776b27fca00ae6aecff346890d65
CENITTOM\0101220 <bruno.lemos@pt.softinsa.com>
2017-11-13 12:01:41 +0000

    tentative to add native dependencies.

---
+++ fourfeetapp/android/app/src/main/java/com/fourfeet/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package com.fourfeet.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expe5b8aab22be64b2b8c9c0ff9c971a37e://tb-xyw.lemos97.fourfeetapp.exp.direct:80";
+
+}
